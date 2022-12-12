afc8ac6334bee07788a7db17a253b542868dcb5d
emrahcurku <emrahcurkufb@outlook.com>
2022-12-12 17:04:12 +0300

    Second Commit

--- src/main/java/com/example/questapp/LıkeController.java
+++ src/main/java/com/example/questapp/LıkeController.java
@@ -1,12 +1,8 @@
 package com.example.questapp;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PostController {
-
-
-
-
+public class LıkeController {
 }
+
